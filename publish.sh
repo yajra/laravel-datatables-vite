0e189e44591df6b41acb2365e6feb428d1ab0528
Arjay Angeles <aqangeles@gmail.com>
2022-10-14 16:08:30 +0800

    Bump 0.1.0 :rocket:

--- publish.sh
+++ publish.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-version=0.0.0
+version=0.1.0
 
 echo "Publishing version $version"
 
 git add .
 git commit -am "Bump $version :rocket:"
 npm version $version
-git push origin master && git push origin master --tags
+git push origin main && git push origin main --tags
 
 npm publish
